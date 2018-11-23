4c756b4da50d348716cb2d5aa8278cc3007cfe70
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2018-11-23 19:38:15 +0100

    Union decoders

--- src/App.fs
+++ src/App.fs
@@ -50,13 +50,20 @@ let UNION_CASE_NO_FIELDS_DECODER_TEMPLATE = """
             | "[NAME]" -> Decode.succeed [NAME]"""
 
 let UNION_CASE_DECODER_TEMPLATE = """
-            | "[NAME]" ->  Decode.map[FIELD_COUNT] (fun [FIELD_CURRIED] -> [NAME]([FIELD_TUPLED])) [FIELD_DECODERS]"""
+            | "[NAME]" -> Decode.map[FIELD_COUNT] [FIELD_CONSTRUCTOR] [FIELD_DECODERS]"""
 
 let EXTENSION_TEMPLATE = """
 type [NAME] with[ENCODER][DECODER]"""
 
 let BUILT_IN_TYPES =
-    Set ["string"; "int"; "float"; "decimal"]
+    Map [
+        "string", "string"
+        "int", "int"
+        "float", "float"
+        "decimal", "decimal"
+        "DateTime", "datetime"
+        "DateTimeOffset", "datetimeOffset"
+    ]
 
 let log x = Browser.console.log(x)
 
@@ -99,12 +106,14 @@ let (|Lines|) f txt =
         cleanComments line |> Option.bind f)
     |> Array.toList
 
-let (|List|Option|Unknown|) = function
-    | MatchGroups @"(\w+) (\w+)" (_, [typ; generic]) as txt ->
-        match generic with
+let (|List|Option|Array|Map|Unknown|) = function
+    | MatchGroups @"(\w+)(\[\]| \w+)" (_, [typ; generic]) as txt ->
+        match generic.Trim() with
         | "list" -> List typ
         | "option" -> Option typ
+        | "[]" | "array" -> Array typ
         | _ -> Unknown txt
+    | MatchGroups @"Map<(\w+),\s*(\w+)>" (_, [gen1; gen2]) -> Map(gen1, gen2)
     | txt -> Unknown txt
 
 let splitByColon (txt: string) =
@@ -138,23 +147,27 @@ let findUnions = function
 
 let printDecoderCall typ =
     let printType typ =
-        if BUILT_IN_TYPES.Contains typ
-        then "Decode." + typ
-        else typ + ".Decoder"
+        match Map.tryFind typ BUILT_IN_TYPES with
+        | Some typ -> "Decode." + typ
+        | None -> typ + ".Decoder"
     match typ with
     | List typ -> sprintf "(Decode.list %s)" (printType typ)
+    | Array typ -> sprintf "(Decode.array %s)" (printType typ)
     // TODO: Make the field optional?
     | Option typ -> sprintf "(Decode.option %s)" (printType typ)
+    | Map(_typ1, typ2) -> sprintf "(Decode.dict %s)" (printType typ2)
     | Unknown typ -> printType typ
 
 let printEncoderCall typ =
     let printType typ =
-        if BUILT_IN_TYPES.Contains typ
-        then "Encode." + typ
-        else typ + ".Encode"
+        match Map.tryFind typ BUILT_IN_TYPES with
+        | Some typ -> "Encode." + typ
+        | None -> typ + ".Encode"
     match typ with
-    | List typ -> sprintf "(Encode.list %s)" (printType typ)
-    | Option typ -> sprintf "(Encode.option %s)" (printType typ)
+    | List _typ -> "Encode.list"
+    | Array _typ -> "Encode.array"
+    | Option typ -> sprintf "Encode.option %s" (printType typ)
+    | Map(_typ1, _typ2) -> "Encode.dict"
     | Unknown typ -> printType typ
 
 let printRecordDecoder name (fields: (string*string) list) =
@@ -168,12 +181,12 @@ let printRecordDecoder name (fields: (string*string) list) =
         .Replace("[NAME]", name)
         .Replace("[FIELDS]", fields)
 
-let printRecordCoder (coderTemplate: string) (fieldTemplate: string) name (fields: (string*string) list) =
+let printRecordCoder (coderTemplate: string) (fieldTemplate: string) coderCall name (fields: (string*string) list) =
     let fields =
         fields |> List.map (fun (name, typ) ->
             fieldTemplate
                 .Replace("[NAME]", name)
-                .Replace("[TYPE]", printDecoderCall typ))
+                .Replace("[TYPE]", coderCall typ))
         |> String.concat ""
     coderTemplate
         .Replace("[NAME]", name)
@@ -182,8 +195,8 @@ let printRecordCoder (coderTemplate: string) (fieldTemplate: string) name (field
 let printRecordExtension name (fields: (string*string) list) =
     EXTENSION_TEMPLATE
         .Replace("[NAME]", name)
-        .Replace("[ENCODER]", printRecordCoder RECORD_ENCODER_TEMPLATE RECORD_FIELD_ENCODER_TEMPLATE name fields)
-        .Replace("[DECODER]", printRecordCoder RECORD_DECODER_TEMPLATE RECORD_FIELD_DECODER_TEMPLATE name fields)
+        .Replace("[ENCODER]", printRecordCoder RECORD_ENCODER_TEMPLATE RECORD_FIELD_ENCODER_TEMPLATE printEncoderCall name fields)
+        .Replace("[DECODER]", printRecordCoder RECORD_DECODER_TEMPLATE RECORD_FIELD_DECODER_TEMPLATE printDecoderCall name fields)
 
 
 let printUnionEncoder name (cases: (string * string list) list) =
@@ -209,11 +222,39 @@ let printUnionEncoder name (cases: (string * string list) list) =
         .Replace("[NAME]", name)
         .Replace("[CASES]", cases)
 
+let printUnionDecoder name (cases: (string * string list) list) =
+    let cases =
+        cases |> List.map (fun (name, fieldTypes) ->
+            match fieldTypes with
+            | [] ->
+                UNION_CASE_NO_FIELDS_DECODER_TEMPLATE.Replace("[NAME]", name)
+            | fs ->
+                let l = List.length fs
+                let fieldCons =
+                    if l = 1 then
+                        name
+                    else
+                        let fsNames = fs |> List.mapi (fun i _ -> sprintf "f%i" (i+1))
+                        "(fun [FIELD_CURRIED] -> [NAME]([FIELD_TUPLED]))"
+                            .Replace("[FIELD_CURRIED]", String.concat " " fsNames)
+                            .Replace("[FIELD_TUPLED]", String.concat ", " fsNames)
+                let decoders = fs |> List.mapi (fun i f ->
+                    sprintf "(Decode.index %i %s)" (i+1) (printDecoderCall f))
+                UNION_CASE_DECODER_TEMPLATE
+                    .Replace("[NAME]", name)
+                    .Replace("[FIELD_COUNT]", if l = 1 then "" else string l)
+                    .Replace("[FIELD_CONSTRUCTOR]", fieldCons)
+                    .Replace("[FIELD_DECODERS]", String.concat " " decoders)
+        ) |> String.concat ""
+    UNION_DECODER_TEMPLATE
+        // .Replace("[NAME]", name)
+        .Replace("[CASES]", cases)
+
 let printUnionExtension name cases =
     EXTENSION_TEMPLATE
         .Replace("[NAME]", name)
         .Replace("[ENCODER]", printUnionEncoder name cases)
-        .Replace("[DECODER]", "")
+        .Replace("[DECODER]", printUnionDecoder name cases)
 
 [<EntryPoint>]
 let run (args: string[]) =
